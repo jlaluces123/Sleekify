546ce8d306cf2895b4ed66b31c870078fd6317bf
Jonathan Laluces <jlaluces@whytespyder.com>
2024-01-18 19:24:41 -0800

    Sessions: implemented next-auth sessions | changed nav and home UI to reflect auth status | log in / log out feature implemented

--- sleekify/src/app/page.js
+++ sleekify/src/app/page.js
@@ -1,9 +1,15 @@
-import Image from 'next/image';
+import { getServerSession } from 'next-auth';
 
-export default function Home() {
+export default async function Home() {
+    const session = await getServerSession();
+
+    // if (session) console.log('Home --> ', session);
+
     return (
         <main className='flex min-h-screen flex-col items-center justify-center p-24'>
-            <div className='font-bold text-4xl'>Hello World</div>
+            <div className='font-bold text-4xl'>
+                {session ? 'You are signed in' : 'You are not signed in'}
+            </div>
         </main>
     );
 }
